5d40952710afe23317819e4a7480718b0d6f4487
codeguy <andywong0425@gmail.com>
2026-06-19 02:20:53 +0800

    Split core content into topic sub-pages per unit

--- sw.js
+++ sw.js
@@ -1,12 +1,21 @@
-const CACHE = 'ces-study-v6';
+const CACHE = 'ces-study-v7';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './styles/main.css',
   './units/unit1-2.html',
+  './units/unit1-2-peer.html',
+  './units/unit1-2-inclusive.html',
   './units/unit1-3.html',
+  './units/unit1-3-money.html',
+  './units/unit1-3-online.html',
+  './units/unit1-3-green.html',
   './units/unit1-4.html',
+  './units/unit1-4-constitution.html',
+  './units/unit1-4-basiclaw.html',
+  './units/unit1-4-ruleoflaw.html',
+  './units/unit1-4-treaties.html',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './5-5/5-5.png'
